2a9fb0d35bda990f8ffc5652b4cdd2f389f124c5
Niraj-Chandravanshi <neerajprasad4u18@gmail.com>
2023-11-11 14:37:29 +0530

    spring boot first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+class Main
+{
+    public static void main(String args[])
+    {
+        int target=3;
+        int i=0;
+        for( String name="a",gender="male";   ; )
+        {
+            if(i>=target)
+            {
+                break;
+            }
+            System.out.println(name+" "+gender);
+            i++;
+        }
+    }
+}
